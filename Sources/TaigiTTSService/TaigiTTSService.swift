2be71d93451418a51a65fd6c5a55ac19a729df45
Li-Heng Hsu <yesleon@me.com>
2021-03-04 15:18:02 +0800

    first commit

--- Sources/TaigiTTSService/TaigiTTSService.swift
+++ Sources/TaigiTTSService/TaigiTTSService.swift
@@ -1,3 +1,47 @@
-struct TaigiTTSService {
-    var text = "Hello, World!"
+//
+//  TaigiTTSService.swift
+//  TaigiTTSService
+//
+//  Created by Li-Heng Hsu on 2021/3/4.
+//
+
+import Foundation
+import AVFoundation
+
+@available(iOS 13.0, OSX 10.15, *)
+public class TaigiTTSService {
+    
+    public static let shared = TaigiTTSService()
+    var subscriptions = [String: Any]()
+    
+    func speechRemoteURL(for text: String) -> URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "xn--lhrz38b.xn--v0qr21b.xn--kpry57d"
+        components.path = "/文本直接合成"
+        components.queryItems = [
+            .init(name: "查詢腔口", value: "台語"),
+            .init(name: "查詢語句", value: text)
+        ]
+        return components.url!
+    }
+    
+    
+    public func preparePlayer(for text: String, completionHandler: @escaping (AVPlayer?) -> Void) {
+        let url = speechRemoteURL(for: text)
+        let asset = AVURLAsset(url: url, options: nil)
+        let playerItem = AVPlayerItem(asset: asset)
+        let player = AVPlayer(playerItem: playerItem)
+        subscriptions[text] = playerItem.publisher(for: \.status)
+            .sink {
+                switch $0 {
+                case .failed:
+                    completionHandler(nil)
+                case .readyToPlay:
+                    completionHandler(player)
+                default:
+                    break
+                }
+            }
+    }
 }
--- Tests/TaigiTTSServiceTests/TaigiTTSServiceTests.swift
+++ Tests/TaigiTTSServiceTests/TaigiTTSServiceTests.swift
@@ -6,7 +6,7 @@ final class TaigiTTSServiceTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TaigiTTSService().text, "Hello, World!")
+//        XCTAssertEqual(TaigiTTSService().text, "Hello, World!")
     }
 
     static var allTests = [
