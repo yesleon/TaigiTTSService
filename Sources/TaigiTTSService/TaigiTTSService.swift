2d881859c640e5a80f6e509c3123dff5e0505ba0
Li-Heng Hsu <yesleon@me.com>
2021-03-15 14:36:52 +0800

    remove combine

--- Sources/TaigiTTSService/TaigiTTSService.swift
+++ Sources/TaigiTTSService/TaigiTTSService.swift
@@ -7,21 +7,18 @@
 
 import Foundation
 import AVFoundation
-import Combine
+
 
 public enum Error: String, Swift.Error {
     case loadingFailure
 }
 
-@available(iOS 13.0, OSX 10.15, *)
+
 public class TaigiTTSService {
     
-    public typealias PreparePlayerPublisher = Publishers.TryCompactMap<NSObject.KeyValueObservingPublisher<AVPlayerItem, AVPlayerItem.Status>, AVPlayer>
+    static var observations = [ObjectIdentifier: NSKeyValueObservation]()
     
-    public static let shared = TaigiTTSService()
-    var subscriptions = Set<AnyCancellable>()
-    
-    func speechRemoteURL(for text: String) -> URL {
+    static func speechRemoteURL(for text: String) -> URL {
         var components = URLComponents()
         components.scheme = "https"
         components.host = "hts.ithuan.tw"
@@ -33,41 +30,23 @@ public class TaigiTTSService {
         return components.url!
     }
     
-    public func preparePlayerPublisher(for text: String) -> PreparePlayerPublisher {
+    public static func player(for text: String, completionHandler: @escaping (AVPlayer?) -> Void) {
         let url = speechRemoteURL(for: text)
         let asset = AVURLAsset(url: url, options: nil)
         let playerItem = AVPlayerItem(asset: asset)
         let player = AVPlayer(playerItem: playerItem)
-        return playerItem.publisher(for: \.status)
-            .tryCompactMap { status -> AVPlayer? in
-                switch status {
-                case .readyToPlay:
-                    return player
-                case .failed:
-                    throw Error.loadingFailure
-                default:
-                    return nil
-                }
+        let identifier = ObjectIdentifier(player)
+        observations[identifier] = playerItem.observe(\.status) { playerItem, change in
+            switch change.newValue {
+            case .failed:
+                completionHandler(nil)
+                observations[identifier] = nil
+            case .readyToPlay:
+                completionHandler(player)
+                observations[identifier] = nil
+            default:
+                break
             }
-            
-    }
-    
-    public func preparePlayer(for text: String, completionHandler: @escaping (AVPlayer?) -> Void) {
-        let url = speechRemoteURL(for: text)
-        let asset = AVURLAsset(url: url, options: nil)
-        let playerItem = AVPlayerItem(asset: asset)
-        let player = AVPlayer(playerItem: playerItem)
-        playerItem.publisher(for: \.status)
-            .sink {
-                switch $0 {
-                case .failed:
-                    completionHandler(nil)
-                case .readyToPlay:
-                    completionHandler(player)
-                default:
-                    break
-                }
-            }
-            .store(in: &subscriptions)
+        }
     }
 }
